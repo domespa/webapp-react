2c8e73e5a955d12af85c98dda34166345d75f5fc
domespa <dumiii1988@gmail.com>
2025-02-25 16:58:50 +0100

    add layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import HomePage from "../pages/HomePage";
-import MoviePage from "../pages/MoviePage";
-import "./App.css";
+// LAYOUT
+import DefaultLayout from "./layouts/DefaultLayout";
+
+// PAGES
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import MoviePage from "./pages/MoviePage";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route index path="/" element={<HomePage />} />
-        <Route path="/movies/:id" element={<MoviePage />} />
-        <Route />
+        <Route element={<DefaultLayout />}>
+          <Route index path="/" element={<HomePage />} />
+          <Route path="/movies/:id" element={<MoviePage />} />
+          <Route />
+        </Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
 }
